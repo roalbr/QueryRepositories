4302a2a26df7517a37fda25367df9526a39943c0
Rodrigo Albuquerque <rodrigoalbuquerque@macbook-air-de-rodrigo.local>
2019-11-29 22:14:03 -0300

    Initial Commit

---
+++ QueryRepositories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QueryRepositories
+//
+//  Created by Rodrigo Albuquerque on 29/11/2019.
+//  Copyright © 2019 Rodrigo Albuquerque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
